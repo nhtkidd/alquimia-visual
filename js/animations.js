1aea2f45a517300367bb3d87fa1407245f912b37
Jesus Mendoza <jesusmdz929@gmail.com>
2024-03-22 00:44:49 -0600

    featur(site):the site has been created

---
+++ js/animations.js
@@ -0,0 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.defaults({
+  scroller: "[data-scroll-container]",
+  markers: false,
+});
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+
+  mobile: {
+    breakpoint: 0,
+    smooth: true,
+  },
+  tablet: {
+    breakpoint: 0,
+    smooth: true,
+  },
+});
